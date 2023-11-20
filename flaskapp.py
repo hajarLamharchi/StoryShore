182ec830e4909a227db8e3d4c8707c5f20eae1b4
hajarLamharchi <125583867+hajarLamharchi@users.noreply.github.com>
2023-11-20 18:44:54 +0100

    adding main.js and flaskapp.py

---
+++ flaskapp.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+@app.route('/home', strict_slashes=False)
+def home():
+    return render_template('home_page.html')
+
+
+@app.route('/login', strict_slashes=False)
+def login():
+    return render_template('login_page.html')
+
+
+@app.route('/register', strict_slashes=False)
+def register():
+    return render_template('signup.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/scripts/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('ul li a').click(function(event) {
+        thisAttrHref = $(this).attr('href');
+        thisAttrHrefOffset = $(thisAttrHref).offset().top;
+        $('html,body').animate({scrollTop:thisAttrHrefOffset});
+        event.preventDefault();
+    });
+});
