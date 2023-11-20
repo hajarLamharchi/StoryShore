16a888030be9b710fd9f43da0eae8233e254e187
ela-hidri <49313184+ela-hidri@users.noreply.github.com>
2023-11-20 23:36:48 +0100

    adding shop and book pages

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,8 +1,12 @@
+
 $(document).ready(function() {
     $('ul li a').click(function(event) {
         thisAttrHref = $(this).attr('href');
         thisAttrHrefOffset = $(thisAttrHref).offset().top;
         $('html,body').animate({scrollTop:thisAttrHrefOffset});
         event.preventDefault();
+       
     });
 });
+
+   
